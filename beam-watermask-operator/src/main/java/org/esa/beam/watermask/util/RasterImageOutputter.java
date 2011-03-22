7796fc393b64eb7568e1e6073788f5d06ca9f092
marpet <marco.peters@brockmann-consult.de>
2011-03-22 16:48:01 +0100

    convert whole directory

--- beam-watermask-operator/src/main/java/org/esa/beam/watermask/util/RasterImageOutputter.java
+++ beam-watermask-operator/src/main/java/org/esa/beam/watermask/util/RasterImageOutputter.java
@@ -16,15 +16,22 @@
 
 package org.esa.beam.watermask.util;
 
+import org.esa.beam.util.io.FileUtils;
+
 import javax.imageio.ImageIO;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -39,23 +46,85 @@ public class RasterImageOutputter {
 
     public static void main(String[] args) throws IOException {
 
-        final InputStream inputStream;
-        if(args.length == 3) {
-            ZipFile zipFile = new ZipFile(args[0]);
-            String shapefile = args[1];
-            final ZipEntry entry = zipFile.getEntry(shapefile);
-            inputStream = zipFile.getInputStream(entry);
+        final File file = new File(args[0]);
+        if (file.isDirectory()) {
+            final File[] imgFiles = file.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(".img");
+                }
+            });
+            final ExecutorService executorService = Executors.newFixedThreadPool(6);
+            for (File imgFile : imgFiles) {
+                final File outputFile = FileUtils.exchangeExtension(imgFile, ".png");
+                if (!outputFile.exists()) {
+                    executorService.submit(new ImageWriterRunnable(imgFile, outputFile));
+                }
+            }
+            while (!executorService.isTerminated()) {
+                try {
+                    executorService.awaitTermination(1000, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            executorService.shutdown();
         } else {
-            inputStream = new FileInputStream(args[0]);
+            final InputStream inputStream;
+            if (args.length == 2) {
+                ZipFile zipFile = new ZipFile(file);
+                String shapefile = args[1];
+                final ZipEntry entry = zipFile.getEntry(shapefile);
+                inputStream = zipFile.getInputStream(entry);
+            } else {
+                inputStream = new FileInputStream(file);
+            }
+            writeImage(inputStream, new File(args[args.length - 1]));
         }
 
-        WritableRaster targetRaster = WritableRaster.createPackedRaster(0, TILE_WIDTH, TILE_WIDTH, 1, 1,
-                                                                        new Point(0, 0));
+        System.exit(0);
+    }
+
+    private static void writeImage(InputStream inputStream, File outputFile) throws IOException {
+        WritableRaster targetRaster = Raster.createPackedRaster(0, TILE_WIDTH, TILE_WIDTH, 1, 1,
+                                                                new Point(0, 0));
 
         final byte[] data = ((DataBufferByte) targetRaster.getDataBuffer()).getData();
         inputStream.read(data);
         final BufferedImage image = new BufferedImage(TILE_WIDTH, TILE_WIDTH, BufferedImage.TYPE_BYTE_BINARY);
         image.setData(targetRaster);
-        ImageIO.write(image, "png", new File(args[args.length - 1]));
+        ImageIO.write(image, "png", outputFile);
+    }
+
+    private static class ImageWriterRunnable implements Runnable {
+
+        private File inputFile;
+        private File outputFile;
+
+        private ImageWriterRunnable(File inputFile, File outputFile) {
+
+            this.inputFile = inputFile;
+            this.outputFile = outputFile;
+        }
+
+        @Override
+        public void run() {
+            InputStream fileInputStream = null;
+            try {
+                fileInputStream = new FileInputStream(inputFile);
+                writeImage(fileInputStream, outputFile);
+                System.out.printf("Written: %s%n", outputFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fileInputStream != null) {
+                    try {
+                        fileInputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
