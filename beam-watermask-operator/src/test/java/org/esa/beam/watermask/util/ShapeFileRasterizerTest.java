1ff4407d42da0751b28f8720fa5fa1c2570eede9
marpet <marco.peters@brockmann-consult.de>
2012-09-26 18:00:34 +0200

    fixed test

--- beam-watermask-operator/src/test/java/org/esa/beam/watermask/util/ShapeFileRasterizerTest.java
+++ beam-watermask-operator/src/test/java/org/esa/beam/watermask/util/ShapeFileRasterizerTest.java
@@ -1,21 +1,21 @@
 package org.esa.beam.watermask.util;
 
-import junit.framework.TestCase;
 import org.esa.beam.watermask.operator.WatermaskUtils;
 import org.junit.Test;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.zip.ZipFile;
 
-public class ShapeFileRasterizerTest extends TestCase {
+import static org.junit.Assert.*;
+
+public class ShapeFileRasterizerTest  {
 
     @Test
-    public void testImageCreation() throws IOException {
+    public void testImageCreation() throws Exception {
         final File targetDir = new File("");
         final ShapeFileRasterizer rasterizer = new ShapeFileRasterizer(targetDir);
         final URL shapeUrl = getClass().getResource("e000n05f.shp");
@@ -25,15 +25,15 @@ public class ShapeFileRasterizerTest extends TestCase {
 
         // test some known values
         final Raster imageData = image.getData();
-        assertEquals(1, imageData.getSample(526, 92, 0));
-        assertEquals(0, imageData.getSample(312, 115, 0));
+        assertEquals(0, imageData.getSample(526, 92, 0));
+        assertEquals(1, imageData.getSample(312, 115, 0));
         assertEquals(1, imageData.getSample(141, 187, 0));
         assertEquals(0, imageData.getSample(197, 27, 0));
         assertEquals(1, imageData.getSample(308, 701, 0));
     }
 
      @Test
-    public void testFromZip() throws IOException {
+    public void testFromZip() throws Exception {
         final File targetDir = new File("");
         final ShapeFileRasterizer rasterizer = new ShapeFileRasterizer(targetDir);
         final URL shapeUrl = getClass().getResource("e000n05f.zip");
@@ -56,8 +56,8 @@ public class ShapeFileRasterizerTest extends TestCase {
 
 //        // test some known values
         final Raster imageData = image.getData();
-        assertEquals(1, imageData.getSample(526, 92, 0));
-        assertEquals(0, imageData.getSample(312, 115, 0));
+        assertEquals(0, imageData.getSample(526, 92, 0));
+        assertEquals(1, imageData.getSample(312, 115, 0));
         assertEquals(1, imageData.getSample(141, 187, 0));
         assertEquals(0, imageData.getSample(197, 27, 0));
         assertEquals(1, imageData.getSample(308, 701, 0));
